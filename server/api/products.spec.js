707b50f290fe90f9e3c8af5e132c88857e591371
Kiara Anderson <kiara.m.anderson@gmail.com>
2019-11-05 15:46:48 -0500

    started product api specs

---
+++ server/api/products.spec.js
@@ -0,0 +1,32 @@
+/* global describe beforeEach it */
+
+const {expect} = require('chai')
+const request = require('supertest')
+const db = require('../db')
+const app = require('../index')
+const Product = db.model('product')
+
+describe('Product routes', () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+
+  describe('/api/products/', () => {
+    const itemName = 'slingshot'
+
+    beforeEach(() => {
+      return Product.create({
+        name: itemName
+      })
+    })
+
+    it('GET /api/products', async () => {
+      const res = await request(app)
+        .get('/api/products')
+        .expect(200)
+
+      expect(res.body).to.be.an('array')
+      expect(res.body[0].name).to.be.equal(itemName)
+    })
+  }) // end describe('/api/products')
+}) // end describe('Product routes')
